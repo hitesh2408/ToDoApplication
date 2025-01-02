411f475e4c0a900eebc9588fbaf3fc88b5174ea5
Hitesh Vaidya <vaidya.hitesh1707@gmail.com>
2025-01-02 17:48:12 +0530

    add choices

---
+++ toDoApp/toDoApp/choices.py
@@ -0,0 +1,11 @@
+"""File to store all the choices for To Do Application."""
+
+
+class TaskStatus:
+    """Class to define all the choices."""
+
+    NOT_STARTED = 0
+    IN_PROGRESS = 1
+    UNDER_REVIEW = 2
+    COMPLETED = 3
+    DEFERRED = 4
