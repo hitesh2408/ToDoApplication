c308612480f1652727261a0762a55d53297d1e53
Hitesh Vaidya <vaidya.hitesh1707@gmail.com>
2025-01-06 20:08:44 +0530

    add choices

---
+++ toDoApp/base/choices.py
@@ -0,0 +1,10 @@
+"""File to have base for all choices."""
+
+
+class UtilityChoices:
+    """Class for Utility choices."""
+
+    NOT_APPLICABLE = "NA"
+    DAILY_USE = "DU"
+    MATHEMATICAL = "MT"
+    FILE_HANDLING = "FH"
