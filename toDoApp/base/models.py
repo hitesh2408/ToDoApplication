94e0e391f752e156969826fe743f51db77d7da07
Hitesh Vaidya <vaidya.hitesh1707@gmail.com>
2025-01-06 20:09:08 +0530

    add base models

--- toDoApp/base/models.py
+++ toDoApp/base/models.py
@@ -1,3 +1,22 @@
+"""File to create base for all models."""
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from base.choices import UtilityChoices
+
+class AuditModel:
+    """Fields for audit purpose."""
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    modified_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Utilities:
+    """Model for all the Utilities."""
+    utility_name = models.CharField(max_length=50)
+    utility_description = models.TextField()
+    type = models.CharField(
+        max_length=5, choices=UtilityChoices, default=UtilityChoices.NOT_APPLICABLE
+    )
