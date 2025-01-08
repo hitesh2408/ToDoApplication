ecb8653c819173480876dca52530563997265012
Hitesh Vaidya <vaidya.hitesh1707@gmail.com>
2025-01-08 20:23:51 +0530

    add utility handler

---
+++ toDoApp/toDoApp/handlers/utility_handler.py
@@ -0,0 +1,23 @@
+"""Handler for all utlities."""
+
+
+from base.choices import UtilityStatus
+from base.models import Utilities
+
+
+class UtilityHandler:
+    """Class for Utility Handler."""
+
+    def __init__(self):
+        """Initialize the class."""
+        pass
+
+    def get_all_active_utlities(self):
+        """Return all utility details."""
+        utilities = Utilities.objects.filter(
+            #utility_status=UtilityStatus.ACTIVE
+        ).values(
+            'utility_name', 'utility_description', 'type', 'utility_status'
+        )
+
+        return utilities
